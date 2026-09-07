e0d8e6829af9db5780e5f4ab32823e791da89868
Vignesh Masani <144472949+itzmevig@users.noreply.github.com>
2026-09-07 13:35:57 +0530

    problem_2.py

--- problem_2.py
+++ problem_2.py
@@ -3,10 +3,12 @@ a=list(map(int,input().split()))
 m=int(input())
 m_sum=0
 for i in range(m-1,n):
-  sp=a[i-2:i+1]
+  sp=a[i+1-m:i+1]
   sum=0
   for j in sp:
     sum+=j
   if sum>m_sum:
     m_sum=sum
 print(m_sum)
+
+       
